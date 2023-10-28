575dd0b3985c52f5c0c2be8b4b3bf2f9e4061055
Késia Silva <kesiasilvab6@gmail.com>
2023-10-28 17:51:18 -0300

    Update 7questoes-emC.c

--- 7questoes-emC.c
+++ 7questoes-emC.c
@@ -4,13 +4,14 @@
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    int q = 1, certo = 0;
+    int q = 0, certo = 0, errado = 0, fim;
     char alternativa;
 
     printf("QUESTOES:\n");
-
-    do{
-        printf("questao %i : \n", q);
+        do{
+        //questao 1//
+        printf("\n--------------------\n");
+        printf("questao 1: \n");
         printf("Qual a sintaxe correta para daclarar uma variavel inteira em C?\n");
         printf("a) int x;\n");
         printf("b) var x;\n");
@@ -24,11 +25,12 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
-        
+            errado++;
         }
+          printf("\n--------------------\n");
         q++;
-
-        printf("questao %i : \n", q);
+        //questao 2//
+        printf("questao 2: \n");
         printf("Qual é a função usada para imprimir no console em C?\n");
         printf("a) print();\n");
         printf("b) console.log();\n");
@@ -42,11 +44,12 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
-        
+            errado++;
         }
+           printf("\n--------------------\n");
         q++;
-
-        printf("questao %i : \n", q);
+        //questao 3//
+        printf("questao 3: \n");
         printf("O que o operador '==' faz em C? \n");
         printf("a) Atribui um valor a uma variável.\n");
         printf("b) Compara dois valores.\n");
@@ -60,11 +63,12 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
+            errado++;
         }
+           printf("\n--------------------\n");
         q++;
-
-        
-        printf("questao %i : \n", q);
+        //questao 4//
+        printf("questao 4: \n");
         printf("Qual é o operador lógico para 'OU' em C?\n");
         printf("a) &&\n");
         printf("b) ||\n");
@@ -78,10 +82,12 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
+            errado++;
         }
+           printf("\n--------------------\n");
         q++;
-
-        printf("questao %i : \n", q);
+        //questao 5//
+        printf("questao 5: \n");
         printf("Qual é a estrutura de controle usada para repetição em C?\n");
         printf("a) if\n");
         printf("b) switch\n");
@@ -95,10 +101,12 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
+            errado++;
         }
+           printf("\n--------------------\n");
         q++;
-
-        printf("questao %i : \n", q);
+        //questao 6//
+        printf("questao 6: \n");
         printf("Qual é a função usada para ler um número inteiro do teclado em C?\n");
         printf("a) read_integer();\n");
         printf("b) input_int();\n");
@@ -112,12 +120,13 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
+            errado++;
         }
+           printf("\n--------------------\n");
         q++;
-
-        
-        printf("questao %i : \n", q);
-        printf("Questão 10: Qual é o valor da expressão 5 + 3 * 2 em C?\n");
+        //questao 7//
+        printf("questao 7: \n");
+        printf("Qual é o valor da expressão 5 + 3 * 2 em C?\n");
         printf("a) 16\n");
         printf("b) 11\n");
         printf("c) 13\n");
@@ -130,10 +139,25 @@ int main(){
         }
         else{
             printf("alternativa errada\n");
+            errado++;
         }
-        
-    }while (q < 7);
-    
-      printf("voce acertou %i questoes", certo);  
-    
+           printf("\n--------------------\n");
+          q++;
+      //printar na tela quantos erros e acertos//
+        printf("Voce acertou %i questoes", certo);  
+        printf("\nVoce errou %i questoes", errado);
+        //tentar novamente ou sair!//
+        printf("\nSe deseja melhorar a nota digite 1, ou sair do progama digite 2: ");
+        scanf("%i", &fim);
+            switch (fim){
+                case 1:
+                printf("Melhorando a nota!\n");
+                break;
+            }
+        }while (fim == 1);
+  //fim do progama//
+  printf("\n--------------------\n");
+  printf("numero de tentativas: %i e acertos: %i", q, certo);
+  printf("\nObrigado!");
+  
 }
